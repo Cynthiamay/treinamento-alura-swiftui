51de7988a79e3e08a05f33d213e1abcc3571d1c7
Mateus Amorim <amorim.mateussantos@outlook.com.br>
2024-06-17 19:36:08 -0300

    chamada api

--- AluraSwiftUI/App/ContentView.swift
+++ AluraSwiftUI/App/ContentView.swift
@@ -22,6 +22,27 @@ struct ContentView: View {
                 })
             }
         }
+        
+        .onAppear {
+            fetchData()
+        }
+    }
+    
+    func fetchData() {
+        guard let url = URL(string: "https://private-c25a8b-cynthiamay.apiary-mock.com/home") else { return }
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data) as? [[String:Any]]
+                    print(json)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }.resume()
     }
 }
 
